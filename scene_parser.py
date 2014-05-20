a1ce0380b1dd39167a4fd4b2e86d621a2944ff23
David Blanchet <david.blanchet5@gmail.com>
2014-05-20 22:56:27 +0200

    Simplify external scene description format and parser

--- scene_parser.py
+++ scene_parser.py
@@ -1,4 +1,3 @@
-#! /usr/bin/python
 
 
 def parse_scene_setup(data, namespace):
@@ -11,25 +10,16 @@ def parse_scene_setup(data, namespace):
         except TypeError:
             return json_obj
 
-        # Get constructor arguments.
-        kwargs = {}
-        for key, value in json_obj.items():
-            if key == 'type':
-                continue
-            kwargs[key] = parse(value)
+        # Remove now useless type information.
+        del json_obj['type']
+
+        # Parse arguments if necessary.
+        kwargs = {key: parse(value) for key, value in json_obj.items()}
 
         # Create instance.
         return namespace[classname](**kwargs)
 
-    def parse_named(json_obj):
-
-        # First level object are named.
-        obj_name, content = json_obj.items()[0]
-        instance = parse(content)
-        instance.name = obj_name
-        return instance
-
-    lights = [parse_named(obj) for obj in data['lights']]
-    objects = [parse_named(obj) for obj in data['objects']]
+    lights = [parse(obj) for obj in data['lights']]
+    objects = [parse(obj) for obj in data['objects']]
 
     return lights, objects
